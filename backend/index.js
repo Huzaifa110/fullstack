046f7bd04608399e39cf2267a2aef02d6993be18
Muhammad Huzaifa <114142659+Huzaifa110@users.noreply.github.com>
2024-04-28 14:19:46 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,18 @@ const port = 8000;
 const mongoDB = require("./db");
 mongoDB();
 
+app.use(cors(
+  {
+    origin: ["https://fullstack-api-two.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+));
+
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Method", 'GET, POST, PUT, DELETE');
+  res.header("Access-Control-Allow-Methods", 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
