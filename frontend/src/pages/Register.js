fb4a880bab8a3d6ba497ef8ac7f97b0db8d2a4aa
Muhammad Huzaifa <114142659+Huzaifa110@users.noreply.github.com>
2024-04-28 14:17:07 +0500

    Update Register.js

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -12,7 +12,7 @@ function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:8000/api/register", {
+      const response = await fetch("https://fullstack-api-two.vercel.app/register", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
