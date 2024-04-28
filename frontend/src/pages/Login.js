ab0317f81fc003d42ef05d60c1f1ad3bf19f4ed9
Muhammad Huzaifa <114142659+Huzaifa110@users.noreply.github.com>
2024-04-28 14:16:29 +0500

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -12,7 +12,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:8000/api/login", {
+      const response = await fetch("https://fullstack-api-two.vercel.app/login", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
